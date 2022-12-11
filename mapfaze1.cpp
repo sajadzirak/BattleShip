fabb28aa0ca49a15be22d260aead0ee961dce4eb
reza-ma82 <120109885+reza-ma82@users.noreply.github.com>
2022-12-11 09:35:38 +0330

    Update mapfaze1.cpp
    separation of player 1 and player 2 maps

--- mapfaze1.cpp
+++ mapfaze1.cpp
@@ -4,17 +4,30 @@ int main()
 {
     int i, j, n;
     scanf("%d", &n);
-    int map[100][100];
+    int mapPlayer1[100][100];
+    int mapPlayer2[100][100];
     for (i = 0, j = 0; j < n; j++)
     {
-        map[i][j] = j + 1;
+        mapPlayer1[i][j] = j + 1;
     }
     for (i = 1; i <= n; i++)
     {
-        map[i][0] = i;
+        mapPlayer1[i][0] = i;
         for (j = 1; j < n; j++)
         {
-            map[i][j] = 0;
+            mapPlayer1[i][j] = 0;
+        }
+    }
+    for (i = 0, j = 0; j < n; j++)
+    {
+        mapPlayer2[i][j] = j + 1;
+    }
+    for (i = 1; i <= n; i++)
+    {
+        mapPlayer2[i][0] = i;
+        for (j = 1; j < n; j++)
+        {
+            mapPlayer2[i][j] = 0;
         }
     }
 
@@ -23,7 +36,7 @@ int main()
         // p1 map
         for (j = 0; j <= n; j++)
         {
-            switch (map[i][j])
+            switch (mapPlayer1[i][j])
             {
             case 0:
                 printf("~ ");
@@ -32,15 +45,22 @@ int main()
                 // for bombs
                 break;
             default:
+            if (n>9)
+            {
                 if (i > 9)
                 {
-                    printf("%d", map[i][j]);
+                    printf("%d ", mapPlayer1[i][j]);
                 }
                 else
                 {
-                    printf("%d ", map[i][j]);
+                    printf("%d  ", mapPlayer1[i][j]);
                 }
-                break;
+            }else
+            {
+                printf("%d ", mapPlayer1[i][j]);
+            }
+            
+            break;
             }
         }
         // space between p1 and p2
@@ -49,7 +69,7 @@ int main()
         // p2 map
         for (j = 0; j <= n; j++)
         {
-            switch (map[i][j])
+            switch (mapPlayer2[i][j])
             {
             case 0:
                 printf("~ ");
@@ -58,29 +78,43 @@ int main()
                 // for bombs
                 break;
             default:
+            if (n>9)
+            {
                 if (i > 9)
                 {
-                    printf("%d", map[i][j]);
+                    printf("%d ", mapPlayer2[i][j]);
                 }
                 else
                 {
-                    printf("%d ", map[i][j]);
+                    printf("%d  ", mapPlayer2[i][j]);
                 }
-                break;
+            }else
+            {
+                printf("%d ", mapPlayer2[i][j]);
+            }
+            break;
             }
         }
         printf("\n");
     }
-    printf(" ");
+    if (n>9)
+    {
+        printf("  ");
+    }
+    else
+    {
+        printf(" ");
+    }
+    
     // numbers at the bottom of the graph p1
     for (i = 0, j = 0; j < n; j++)
     {
-        printf(" %d", map[i][j]);
+        printf(" %d", mapPlayer1[i][j]);
     }
     // space between p1 and p2
     if (n > 9)
     {
-        for (i = 1; i < n; i++)
+        for (i = 0; i < n; i++)
             printf(" ");
     }
     else
@@ -91,6 +125,6 @@ int main()
     // numbers at the bottom of the graph p2
     for (i = 0, j = 0; j < n; j++)
     {
-        printf(" %d", map[i][j]);
+        printf(" %d", mapPlayer2[i][j]);
     }
 }
