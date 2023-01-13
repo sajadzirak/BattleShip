67ba7636ae6b8781f15f6f27959992b71061b4df
reza-ma82 <120109885+reza-ma82@users.noreply.github.com>
2023-01-13 23:28:58 +0330

    change location of new functions

--- gameData.h
+++ gameData.h
@@ -32,4 +32,51 @@ struct ships
     int sw[3] = {1, 1, 1};
 } shipP1[15], shipP2[15];
 
+void saveGame() {
+    FILE* saveGameData = fopen("gameData.dat", "wb");
+    fwrite(&n, sizeof(int), 1, saveGameData);
+    fwrite(&nship, sizeof(int), 1, saveGameData);
+    fwrite(&gameType, sizeof(int), 1, saveGameData);
+    fwrite(&playerTurn, sizeof(int), 1, saveGameData);
+    fwrite(mapPlayer1, sizeof(int), 144, saveGameData);
+    fwrite(mapPlayer2, sizeof(int), 144, saveGameData);
+    fwrite(namePlayer1, sizeof(char), 20, saveGameData);
+    fwrite(namePlayer2, sizeof(char), 20, saveGameData);
+    fwrite(&shipPlayer1, sizeof(int), 1, saveGameData);
+    fwrite(&shipPlayer2, sizeof(int), 1, saveGameData);
+    fwrite(saveHits, sizeof(int), 169, saveGameData);
+    fwrite(around, sizeof(int), 4, saveGameData);
+    fwrite(&hitsw, sizeof(int), 1, saveGameData);
+    fwrite(hitPos, sizeof(int), 2, saveGameData);
+    fwrite(prevHit, sizeof(int), 2, saveGameData);
+    fwrite(&soundEffectSw, sizeof(int), 1, saveGameData);
+    fwrite(shipP1, sizeof(ships), 15, saveGameData);
+    fwrite(shipP2, sizeof(ships), 15, saveGameData);
+    fclose(saveGameData);
+}
+//-------------------------------------------------------------------
+
+void loadGameData() {
+    FILE* saveGameData = fopen("gameData.dat", "rb");
+    fread(&n, sizeof(int), 1, saveGameData);
+    fread(&nship, sizeof(int), 1, saveGameData);
+    fread(&gameType, sizeof(int), 1, saveGameData);
+    fread(&playerTurn, sizeof(int), 1, saveGameData);
+    fread(mapPlayer1, sizeof(int), 144, saveGameData);
+    fread(mapPlayer2, sizeof(int), 144, saveGameData);
+    fread(namePlayer1, sizeof(char), 20, saveGameData);
+    fread(namePlayer2, sizeof(char), 20, saveGameData);
+    fread(&shipPlayer1, sizeof(int), 1, saveGameData);
+    fread(&shipPlayer2, sizeof(int), 1, saveGameData);
+    fread(saveHits, sizeof(int), 169, saveGameData);
+    fread(around, sizeof(int), 4, saveGameData);
+    fread(&hitsw, sizeof(int), 1, saveGameData);
+    fread(hitPos, sizeof(int), 2, saveGameData);
+    fread(prevHit, sizeof(int), 2, saveGameData);
+    fread(&soundEffectSw, sizeof(int), 1, saveGameData);
+    fread(shipP1, sizeof(ships), 15, saveGameData);
+    fread(shipP2, sizeof(ships), 15, saveGameData);
+    fclose(saveGameData);
+}
+
 #endif
